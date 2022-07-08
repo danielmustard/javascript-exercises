87f30d9942bcc8b1642d69cbaf990c6de352fe74
danielmustard <daniel.mustard13@gmail.com>
2022-07-09 00:13:58 +0100

    3/7 tests passed on 04 needs work

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    
+    for (const element of args){ //for each element within a possible array of arguments 
 
+        const indexToRemove = array.indexOf(element); // we select the index of the current element we are looking at and then slice that index
+                                                    //with the code below
+        
+        if (indexToRemove > -1){
+            let splicedIndex = array.splice(indexToRemove, 1);
+            console.log(array);
+            return array;
+        };
+    };
+
+
+  
 };
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
