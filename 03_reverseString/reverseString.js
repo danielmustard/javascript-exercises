266786b5bae90c8cc84265550c7a7d5e8f6e5dcf
danielmustard <daniel.mustard13@gmail.com>
2022-07-08 22:29:04 +0100

    completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    //first we must split name so each letter is a member of array 
+    const splitString = inputString.split('')
+    //the split name now holds the value like this, for example name 'dan' ['d', 'a', 'n']
+    //Now we reverse the array
+    const reverseString = splitString.reverse()
+    
+    //Now the array looks like this, for example name 'dan' ['n','a','d']
+    
+    //we can then return the array as a normal string with the .join function 
 
+    return reverseString.join('');
+    
 };
 
 // Do not edit below this line
