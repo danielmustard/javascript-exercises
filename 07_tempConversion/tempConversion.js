6e9c592c6e8d2cc7c92fc04cf70d8f0e979e653a
Daniel Mustard <danielmustard1@icloud.com>
2022-07-11 15:09:38 +0100

    07 complete, now able to convert celcius to farenheit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  
+  celciusValue = (f-32) * 5/9;
+  return parseFloat(celciusValue.toFixed(1));
 };
-
-const ctof = function() {
-
+//parse float means the float value will not be included if the 
+const ctof = function(c) {
+  fahrenheitValue = (c * 9/5) + 32;
+  return parseFloat(fahrenheitValue.toFixed(1));
 };
 
 // Do not edit below this line
