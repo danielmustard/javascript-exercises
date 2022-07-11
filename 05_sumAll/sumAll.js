ddd2f0210a43b0f2cf84f3359758947dd3d3b930
Daniel Mustard <danielmustard1@icloud.com>
2022-07-11 09:38:35 +0100

    got 4/6 tests working on 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(value1, value2) {
+    let overallAddition = 0;
+    if (Number.isInteger(value1) == true && Number.isInteger(value2) == true){
+       for (let i = value1; i <= value2; i++){
+        overallAddition += i;
+       } 
+       return overallAddition;
+    }else{
+        return ('ERROR')
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
