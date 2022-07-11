fd2befef872a2de0e42f2053ddf2cc7756451d25
Daniel Mustard <danielmustard1@icloud.com>
2022-07-11 13:46:30 +0100

    all checks on 06 passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -4,8 +4,10 @@ const leapYears = function(year) {
         return true;
     }else if (year % 100 == 0){ //Any year that can be devided by 100 is not a leap year.......
         return false;
-    }else if (year % 4 == 0){
+    }else if (year % 4 == 0){ //if year can be devided by four after above checks are done then it is a leap year.
         return true;
+    }else{ //in any other cases where above rules cannot be met we can just return false! 
+        return false;
     }
 
 };
