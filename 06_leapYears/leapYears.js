9a90d800059667deb9bf8262883574e678ffb067
Daniel Mustard <danielmustard1@icloud.com>
2022-07-11 13:43:31 +0100

    got 3/6 tests to pass on 06 leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if (year % 100 == 0 && year % 400 == 0){ //if year can be devided by 100 and 400 then it is a leap year
+        return true;
+    }else if (year % 100 == 0){ //Any year that can be devided by 100 is not a leap year.......
+        return false;
+    }else if (year % 4 == 0){
+        return true;
+    }
 
 };
 
