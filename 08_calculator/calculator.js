63aa2798303d31f3fa6d1f174d359790a9ea4c90
Daniel Mustard <danielmustard1@icloud.com>
2022-07-12 11:53:37 +0100

    got most working on 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,20 +10,27 @@ const subtract = function(...args) {
   return args[0] - args[1];    
 };
 
-const sum = function() {
+const sum = function(args) {
   let total = 0;
-  args.forEach(element => {
-    total += element;
-    });
+    args.forEach(element => {
+      //console.log(element)
+      total += element;
+      });
+      return total;
 	
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  const arrayLenght = args.length;
+  let total = 1; 
+  for (var i =0; i < arrayLenght; i++){
+    total *= args[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(value1, value2) {
+  return value1 ** value2;
 };
 
 const factorial = function() {
