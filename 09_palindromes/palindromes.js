58b428963a0a1f1bdca09c6b8f7c5cf4aad084fc
Daniel Mustard <danielmustard1@icloud.com>
2022-07-12 16:16:40 +0100

    started 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (input) {
 
+    //for this we will put each letter of the string into an array, we will then reverse the array and compare it to the original array, if they match its a palidrome
+  
+    let inputArray = input.split(''); //here we have split the input up into an array
+    let revArray = input.split('').reverse(); // this is our inputted array but reversed
+  
+    if (JSON.stringify(inputArray) === JSON.stringify(revArray)){ //we cannot compare two arrays as if they were strings so we need to use JSON.sringify to make the array behave like a string so we can compare them.
+      return true;
+    }else if (JSON.stringify(inputArray) != JSON.stringify(revArray)){
+      return false;
+    }else {
+      return ('ERROR');
+    }
 };
 
 // Do not edit below this line
