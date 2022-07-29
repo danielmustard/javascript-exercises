7c715768db40ac6b6fb09c11ecb958978566ad23
danielmustard <danielmustard1@icloud.com>
2022-07-29 16:02:33 +0100

    palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,17 +1,21 @@
 const palindromes = function (input) {
+  // \W is used to find any none word characters, so we can use this to sanitize our input.
+  const noChar = /[\W_]/g;
+  //Removing any none characters
+  const sanitizedInput = input.toLowerCase().replace(noChar,"");
+  //Reversing the input string 
+  let revString = sanitizedInput.split('').reverse().join('') // this is our input reversed
 
-    //for this we will put each letter of the string into an array, we will then reverse the array and compare it to the original array, if they match its a palidrome
-  
-    let inputArray = input.split(''); //here we have split the input up into an array
-    let revArray = input.split('').reverse(); // this is our inputted array but reversed
-  
-    if (JSON.stringify(inputArray) === JSON.stringify(revArray)){ //we cannot compare two arrays as if they were strings so we need to use JSON.sringify to make the array behave like a string so we can compare them.
-      return true;
-    }else if (JSON.stringify(inputArray) != JSON.stringify(revArray)){
-      return false;
-    }else {
-      return ('ERROR');
-    }
+  //console.log(sanitizedInput);
+  //console.log(revString);
+
+  if (revString === sanitizedInput){
+    return true;
+  }else if (revString != sanitizedInput){
+    return false;
+  }else{
+    return 'ERROR'
+  };
 };
 
 // Do not edit below this line
