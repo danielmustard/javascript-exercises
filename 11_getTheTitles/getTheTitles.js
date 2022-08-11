a825fffafdd4e18858c7b0e2a4198a69f6f21b5f
danielmustard <daniel.mustard13@gmail.com>
2022-08-11 19:04:50 +0100

    11 get the titles done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    bookTitles = [];
+
+  //For each item within the passed in array we get the book title and add it to our own array called [bookTitles]
+  books.forEach(book => {
+      //we add each book title to our array
+    bookTitles.push(book.title);
+  });
+  //we return the array once the for each loop is done
+  return bookTitles;
 
 };
 
